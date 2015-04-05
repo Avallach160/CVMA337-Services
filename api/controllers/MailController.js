d740a82f3a9f81a057a11c86d3b3cc636ea62994
Avallach160 <cbaird@kairyt.com>
2015-04-05 16:52:23 -0700

    email work

--- api/controllers/MailController.js
+++ api/controllers/MailController.js
@@ -1,22 +1,38 @@
 var nodemailer = require('nodemailer');
 var local = require('../../config/local.js');
+var EventEmitter = require('events').EventEmitter;
+
 module.exports = {
 	send: function(req, res){
+		var un = local.cvmaMailUser;
+		var pw = local.cvmaMailPw;
+
 		var transporter = nodemailer.createTransport({
-			service: 'gmail',
+			// service: 'gmail',
+			host: 'smtp.gmail.com',
+			port: 587,
+			secure: false,
 			auth: {
-				user: local.cmvaMailUser,
-				pass: local.cvmaMailPw
-			}
+				user: un,
+				pass: pw
+			},
+			debug: false
 		});
+
+		//use when debug:true
+		//transporter.on('log', function(data){
+		//	console.log(data);
+		//});
+
 		transporter.sendMail({
-		    from: 'cvma337@gmail.com',
+		    from: 'cvma33.7@gmail.com',
 		    to: req.body.to,
 		    subject: req.body.subject,
 		    html: req.body.body
 		}, function(err, success){
+			console.log(err, success);
 			if (err !== null){
-				return res.json({errorSendingMail: err});
+				return res.json(400, {errorSendingMail: err});
 			}
 			return res.json({sent: 'email sent'});
 		});
